88e6cfab32011e46102af733d8ec34c0343c0ea7
isdora-innodems <isdorakinyi01@gmail.com>
2025-01-15 09:56:22 +0300

    add all files required

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,383 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-Whole-Numbers",
+  "level": "1",
+  "url": "sec-Whole-Numbers.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Whole Numbers",
+  "body": " Whole Numbers  Place value of hundreds of millions   Work in groups  (i) Write any eight digit-number  (ii) Represent the numbers in the place value chart as ahown below.   Place Value    Place Values  Digit    Ten millions     Millions     Hundred thousands     Ten thousands     Thousands     Hundreds     Tens     Ones      (iii) Discuss and share with other groups    Learing point       is one hundred million.   Work in pairs.  (i) Write any nine-digit number.  (ii) Represent the number in a place value chart shown.      Place Values  Digit    Hundred millions(Hm)     Ten millions(Tm)     Millions(M)     Hundred thousands(Ht)     Ten thousands(Tt)     Thousands(T)     Hundreds(H)     Tens(T)     Ones(O)      Write the place value of each digit in the number.   Learning point  The 9th digits in hundred millions place value.   Use a place value chart to identify and write the place value of each digit in the number       Place Values  Digit    Hundred millions(Hm)  5    Ten millions(Tm)  4    Millions(M)  3    Hundred thousands(Ht)  2    Ten thousands(Tt)  7    Thousands(T)  6    Hundreds(H)  8    Tens(T)  9    Ones(O)  1     Place value of digit is hundred millions.   is ten millions   millions   is hundred thousands   is ten thousands   is thousands   is hundreds   is tens   is ones   "
+},
+{
+  "id": "sec-Whole-Numbers-3",
+  "level": "2",
+  "url": "sec-Whole-Numbers.html#sec-Whole-Numbers-3",
+  "type": "Activity",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Work in groups  (i) Write any eight digit-number  (ii) Represent the numbers in the place value chart as ahown below.   Place Value    Place Values  Digit    Ten millions     Millions     Hundred thousands     Ten thousands     Thousands     Hundreds     Tens     Ones      (iii) Discuss and share with other groups  "
+},
+{
+  "id": "sec-Whole-Numbers-7",
+  "level": "2",
+  "url": "sec-Whole-Numbers.html#sec-Whole-Numbers-7",
+  "type": "Activity",
+  "number": "1.1.2",
+  "title": "",
+  "body": " Work in pairs.  (i) Write any nine-digit number.  (ii) Represent the number in a place value chart shown.      Place Values  Digit    Hundred millions(Hm)     Ten millions(Tm)     Millions(M)     Hundred thousands(Ht)     Ten thousands(Tt)     Thousands(T)     Hundreds(H)     Tens(T)     Ones(O)      Write the place value of each digit in the number.  "
+},
+{
+  "id": "sec-Whole-Numbers-10",
+  "level": "2",
+  "url": "sec-Whole-Numbers.html#sec-Whole-Numbers-10",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "",
+  "body": " Use a place value chart to identify and write the place value of each digit in the number       Place Values  Digit    Hundred millions(Hm)  5    Ten millions(Tm)  4    Millions(M)  3    Hundred thousands(Ht)  2    Ten thousands(Tt)  7    Thousands(T)  6    Hundreds(H)  8    Tens(T)  9    Ones(O)  1     Place value of digit is hundred millions.   is ten millions   millions   is hundred thousands   is ten thousands   is thousands   is hundreds   is tens   is ones  "
+},
+{
+  "id": "sec-Factors",
+  "level": "1",
+  "url": "sec-Factors.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Factors",
+  "body": " Factors     "
+},
+{
+  "id": "sec-Fractions",
+  "level": "1",
+  "url": "sec-Fractions.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Fractions",
+  "body": " Fractions   divide and shade the three circles to show    fractions       Arrange the following fractions in ascending order.       (a) Find the LCM of       2  3  5  4  2    2  3  5  2  1    3  3  5  1  1    5  1  5  1  1    1  1  1  1  1        The LCM of and is   (b) Rewrite the fractions to have a common denominator.                     (c) Arrange the fractions in ascending order.          "
+},
+{
+  "id": "sec-Fractions-2",
+  "level": "2",
+  "url": "sec-Fractions.html#sec-Fractions-2",
+  "type": "Activity",
+  "number": "1.3.1",
+  "title": "",
+  "body": " divide and shade the three circles to show    fractions    "
+},
+{
+  "id": "sec-Fractions-3",
+  "level": "2",
+  "url": "sec-Fractions.html#sec-Fractions-3",
+  "type": "Example",
+  "number": "1.3.1",
+  "title": "",
+  "body": "  Arrange the following fractions in ascending order.       (a) Find the LCM of       2  3  5  4  2    2  3  5  2  1    3  3  5  1  1    5  1  5  1  1    1  1  1  1  1        The LCM of and is   (b) Rewrite the fractions to have a common denominator.                     (c) Arrange the fractions in ascending order.         "
+},
+{
+  "id": "sec-Decimals",
+  "level": "1",
+  "url": "sec-Decimals.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Decimals",
+  "body": " Decimals  Work out:                           Jesica had a string 98.6m long. She cut it into eight equal pieces. How long is each piece.   long division     12.325m    8  98.6      "
+},
+{
+  "id": "sec-Decimals-3-1",
+  "level": "2",
+  "url": "sec-Decimals.html#sec-Decimals-3-1",
+  "type": "Table",
+  "number": "1.4.1",
+  "title": "",
+  "body": "                    "
+},
+{
+  "id": "sec-Decimals-4",
+  "level": "2",
+  "url": "sec-Decimals.html#sec-Decimals-4",
+  "type": "Example",
+  "number": "1.4.2",
+  "title": "",
+  "body": " Jesica had a string 98.6m long. She cut it into eight equal pieces. How long is each piece.   long division     12.325m    8  98.6     "
+},
+{
+  "id": "sec-Squares-and-squareroots",
+  "level": "1",
+  "url": "sec-Squares-and-squareroots.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Squares and squareroots",
+  "body": " Squares and squareroots     "
+},
+{
+  "id": "sec-Algebraic-expressions",
+  "level": "1",
+  "url": "sec-Algebraic-expressions.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Algebraic Expressions",
+  "body": " Algebraic Expressions     "
+},
+{
+  "id": "sec-Linear-expressions",
+  "level": "1",
+  "url": "sec-Linear-expressions.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Linear Expressions",
+  "body": " Linear Expressions   Solving Linear Equations involving addition and subtraction    Solve for in                   Notice that we still have two variables, so instead we multiply both sides of the second equation by two first to get . Now we add this equation to the first:                         "
+},
+{
+  "id": "sec-Linear-expressions-3",
+  "level": "2",
+  "url": "sec-Linear-expressions.html#sec-Linear-expressions-3",
+  "type": "Activity",
+  "number": "2.2.1",
+  "title": "",
+  "body": " Solve for in                 "
+},
+{
+  "id": "sec-Linear-expressions-4",
+  "level": "2",
+  "url": "sec-Linear-expressions.html#sec-Linear-expressions-4",
+  "type": "Example",
+  "number": "2.2.1",
+  "title": "",
+  "body": " Notice that we still have two variables, so instead we multiply both sides of the second equation by two first to get . Now we add this equation to the first:                        "
+},
+{
+  "id": "sec-Linear-inequalities",
+  "level": "1",
+  "url": "sec-Linear-inequalities.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Linear inequalities",
+  "body": " Linear inequalities   Draw the number lines below   Number lines      Linear inequalities are defined as expressions in which two linear expressions are compared using the inequality symbols. The symbols that are used to represent the linear inequalities are listed below:      Symbol name  Symbol  Example    Less than      Greater than      Less than or equal to      Greater than or equal to        The inequality sign always points the smaller number     Using inequality symbols, show how each of the following numbers compare.    (a) 2 and 5  (b) 7 and 5    Solution     (a) 2 is less than 5  (b) 7 is greater than 5    (a) The symbol used should be 2<5  (b) The sybol used should be 7>5      REPRESENTATION OF SIMPLE INEQUALITIES  Inequalities can be represented in a number line.  To plot an inequality such as on a number line, first draw a circle over the number (e.g.,3). Then if the sign includes equal to , fill in the circle. If the sign does not include equal to , leave the circle unfilled in. Finally, draw a line going from the circle in the direction of the numbers that make the inequality true.   Number line      Represent each of the following inequalities in a number line.  (a)   (b)   (a) means is less yhan or equal to 8. It is represented as shown.   Number lines    Notice that the circle is filled to show that 8 is included in the inequality.  (b) means is greater than 5. It is represented as shown   Number lines     "
+},
+{
+  "id": "sec-Linear-inequalities-2",
+  "level": "2",
+  "url": "sec-Linear-inequalities.html#sec-Linear-inequalities-2",
+  "type": "Activity",
+  "number": "2.3.1",
+  "title": "",
+  "body": " Draw the number lines below   Number lines    "
+},
+{
+  "id": "sec-Linear-inequalities-3",
+  "level": "2",
+  "url": "sec-Linear-inequalities.html#sec-Linear-inequalities-3",
+  "type": "Activity",
+  "number": "2.3.2",
+  "title": "",
+  "body": " Linear inequalities are defined as expressions in which two linear expressions are compared using the inequality symbols. The symbols that are used to represent the linear inequalities are listed below:      Symbol name  Symbol  Example    Less than      Greater than      Less than or equal to      Greater than or equal to        The inequality sign always points the smaller number   "
+},
+{
+  "id": "sec-Linear-inequalities-4",
+  "level": "2",
+  "url": "sec-Linear-inequalities.html#sec-Linear-inequalities-4",
+  "type": "Example",
+  "number": "2.3.3",
+  "title": "",
+  "body": " Using inequality symbols, show how each of the following numbers compare.    (a) 2 and 5  (b) 7 and 5    Solution     (a) 2 is less than 5  (b) 7 is greater than 5    (a) The symbol used should be 2<5  (b) The sybol used should be 7>5    "
+},
+{
+  "id": "sec-Linear-inequalities-6",
+  "level": "2",
+  "url": "sec-Linear-inequalities.html#sec-Linear-inequalities-6",
+  "type": "Example",
+  "number": "2.3.4",
+  "title": "",
+  "body": " Represent each of the following inequalities in a number line.  (a)   (b)   (a) means is less yhan or equal to 8. It is represented as shown.   Number lines    Notice that the circle is filled to show that 8 is included in the inequality.  (b) means is greater than 5. It is represented as shown   Number lines    "
+},
+{
+  "id": "sec-Pythagorean-relationship",
+  "level": "1",
+  "url": "sec-Pythagorean-relationship.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Pythagorean relationship",
+  "body": " Pythagorean relationship     "
+},
+{
+  "id": "sec-Length",
+  "level": "1",
+  "url": "sec-Length.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Length",
+  "body": " Length   Converting units of length   Work out:     Unit convrersion     Hm  m  cm     8  20  30    +  2  45  18     10  65  48         Work out the circumference: diameter = 14 cm.   Number lines         Circumference          cm           "
+},
+{
+  "id": "subsec-Converting-units-of-length-2",
+  "level": "2",
+  "url": "sec-Length.html#subsec-Converting-units-of-length-2",
+  "type": "Example",
+  "number": "3.2.1",
+  "title": "",
+  "body": " Work out:     Unit convrersion     Hm  m  cm     8  20  30    +  2  45  18     10  65  48      "
+},
+{
+  "id": "sec-Length-3",
+  "level": "2",
+  "url": "sec-Length.html#sec-Length-3",
+  "type": "Example",
+  "number": "3.2.3",
+  "title": "",
+  "body": " Work out the circumference: diameter = 14 cm.   Number lines         Circumference          cm          "
+},
+{
+  "id": "sec-Area",
+  "level": "1",
+  "url": "sec-Area.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Area",
+  "body": " Area     "
+},
+{
+  "id": "sec-Volume-and-capacity",
+  "level": "1",
+  "url": "sec-Volume-and-capacity.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Volume and capacity",
+  "body": " Volume and capacity     "
+},
+{
+  "id": "sec-Time-distance-speed",
+  "level": "1",
+  "url": "sec-Time-distance-speed.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Time, distance and speed",
+  "body": " Time, distance and speed  Units of measure can be given xml treatment too with the quantity element. In latex , the siunitx  siunitx package  package siunitx  units package is loaded to achive unit handling. Since that package only offers SI units, some other common units will be added by PreTeXt in the preamble. In HTML, the capabilities of siunitx are simulated, weakly. Note that at present, you should not attempt to use the quantity element within a math environment.  The value of gravitational constant is  9.8  . Force is measured in   , also known as one   . A quantity with rather ridiculous units is  23  . One   is the same as   . You can have a unitless quantity, like  42  , which may help with consistency between such numbers and units in the latex output. Some non-SI units are available, such as the absurd   . The latex command \\pi is recognized within mag in conversions to HTML, which is consistent with the behavior with a conversion to latex , for example there are  2\\pi   in a full circle. This is a similar quantity with multiple occurences of \\pi to test a particular template used for HTML output. It is not meant to make any sense:  21\\pi45\\pi234\\pi890   .  For a full list of the allowed units and prefixes, see pretext-units.xsl . If you have a need for more units, they need to be added to pretext-units.xsl in the section that deals with units which are not part of siunitx by default. Note that the mag element should come first, followed by the unit element, followed by the per element.  "
+},
+{
+  "id": "sec-Temperature",
+  "level": "1",
+  "url": "sec-Temperature.html",
+  "type": "Section",
+  "number": "3.6",
+  "title": "Temperature",
+  "body": " Temperature     "
+},
+{
+  "id": "sec-Money",
+  "level": "1",
+  "url": "sec-Money.html",
+  "type": "Section",
+  "number": "3.7",
+  "title": "Money",
+  "body": " Money     "
+},
+{
+  "id": "sec-section-Angles",
+  "level": "1",
+  "url": "sec-section-Angles.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Angles",
+  "body": " Angles   Angles in a straight line   angles on a straight line      Angles at a point   Work out the value of the angle marked in the figure alongside   Agles at a point       Angles on a transverse   Alternate angles  In the figure alongside, work out the values of alternate angles.   alternate angles      Corresponding angles    Work in groups    Draw a pair of parallel lines and a transversal.   Mark the angles as shown below.   Corresponding angles     Cut out angles and using a pair of scissors. Observe safety precautions when using a pair of scissors to avoid injury.   Compare the size of the two angles by placing one on top of the other. What do you notice   Share your findings with other learners in class.  Learning point.   Angles and are corresponding angles. Corresponding angles are equal.     Find the value of angle in the figure below.   Corresponding angles       Co-interior angle   Work in groups   Draw a pair of parallel lines and a transversal   Mark the angles as shown below.   Co-interior angles     Cut the angles and using a pair of scissors   Place the angles on a straight line.  What do you notice?   Share your findings with other learners in class.   Learning point   Angles and are co-interior. Co-interior angles add upto .    What is the value of angle in the figure below?   Co-interior angles       "
+},
+{
+  "id": "subsec-Angles-at-a-point-2",
+  "level": "2",
+  "url": "sec-section-Angles.html#subsec-Angles-at-a-point-2",
+  "type": "Example",
+  "number": "4.1.1",
+  "title": "",
+  "body": " Work out the value of the angle marked in the figure alongside   Agles at a point    "
+},
+{
+  "id": "subsubsec-Corresponding-angles-2",
+  "level": "2",
+  "url": "sec-section-Angles.html#subsubsec-Corresponding-angles-2",
+  "type": "Activity",
+  "number": "4.1.1",
+  "title": "",
+  "body": "  Work in groups    Draw a pair of parallel lines and a transversal.   Mark the angles as shown below.   Corresponding angles     Cut out angles and using a pair of scissors. Observe safety precautions when using a pair of scissors to avoid injury.   Compare the size of the two angles by placing one on top of the other. What do you notice   Share your findings with other learners in class.  Learning point.   Angles and are corresponding angles. Corresponding angles are equal.   "
+},
+{
+  "id": "subsubsec-Corresponding-angles-3",
+  "level": "2",
+  "url": "sec-section-Angles.html#subsubsec-Corresponding-angles-3",
+  "type": "Example",
+  "number": "4.1.2",
+  "title": "",
+  "body": " Find the value of angle in the figure below.   Corresponding angles    "
+},
+{
+  "id": "subsubsec-Co-interior-angle-2",
+  "level": "2",
+  "url": "sec-section-Angles.html#subsubsec-Co-interior-angle-2",
+  "type": "Activity",
+  "number": "4.1.2",
+  "title": "",
+  "body": " Work in groups   Draw a pair of parallel lines and a transversal   Mark the angles as shown below.   Co-interior angles     Cut the angles and using a pair of scissors   Place the angles on a straight line.  What do you notice?   Share your findings with other learners in class.   Learning point   Angles and are co-interior. Co-interior angles add upto .  "
+},
+{
+  "id": "subsubsec-Co-interior-angle-3",
+  "level": "2",
+  "url": "sec-section-Angles.html#subsubsec-Co-interior-angle-3",
+  "type": "Example",
+  "number": "4.1.3",
+  "title": "",
+  "body": " What is the value of angle in the figure below?   Co-interior angles    "
+},
+{
+  "id": "sec-Geometrical-constructions",
+  "level": "1",
+  "url": "sec-Geometrical-constructions.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Geometrical constructions",
+  "body": " Geometrical constructions   Match correctly.    one  moja    two  mbili    three  tatu    "
+},
+{
+  "id": "sec-Geometrical-constructions-2",
+  "level": "2",
+  "url": "sec-Geometrical-constructions.html#sec-Geometrical-constructions-2",
+  "type": "Checkpoint",
+  "number": "4.2.1",
+  "title": "",
+  "body": " Match correctly.    one  moja    two  mbili    three  tatu   "
+},
+{
+  "id": "sec-Data-handling",
+  "level": "1",
+  "url": "sec-Data-handling.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Data handling",
+  "body": " Data handling   pie charts  Draw a pie chart   pie chart     "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
